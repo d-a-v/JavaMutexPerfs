52fa634683c2657d1fe404214f731bd5321f760a
DavidGauchard <gauchard@laas.fr>
2014-09-30 22:44:28 +0200

    simplicity

--- src/CounterAtomic.java
+++ src/CounterAtomic.java
@@ -1,19 +1,14 @@
 import java.util.concurrent.atomic.AtomicLong;
 
-
-public class CounterAtomic implements Counter
+public class CounterAtomic extends AtomicLong implements Counter
 {
-	private AtomicLong counter = new AtomicLong(0);
-	
-	CounterAtomic () { }
-	CounterAtomic (long init) { counter.set(init); }
+	CounterAtomic () { super(0); }
+	CounterAtomic (long init) { super(init); }
 
 	public final String type () { return "AtomicLong"; }
 
-	public long get () { return counter.get(); }
-	public long getIncr () { return counter.getAndIncrement(); }
-	public long incrGet () { return counter.incrementAndGet(); }
-	public long getDecr () { return counter.getAndDecrement(); }
-	public long decrGet () { return counter.decrementAndGet(); }
-	public void set (long newVal) { counter.set(newVal); }
+	public long getIncr () { return getAndIncrement(); }
+	public long incrGet () { return incrementAndGet(); }
+	public long getDecr () { return getAndDecrement(); }
+	public long decrGet () { return decrementAndGet(); }
 }
